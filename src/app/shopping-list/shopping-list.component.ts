aef8a8f57dae22c2031d5b7c409745a3422919a9
shinylei <walei@umich.edu>
2018-09-02 06:58:31 +0800

    enable updatting shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -27,4 +27,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  onEditItem(index: number) {
+    console.log(index);
+    this.slService.startedEditing.next(index);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,7 +7,12 @@ export class ShoppingListService {
         new Ingredient('banana', 6)
     ]
     IngredientChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+    
     getIngredients() {
         return this.ingredients.slice();
     }
@@ -19,6 +24,11 @@ export class ShoppingListService {
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
+        this.IngredientChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
         this.IngredientChanged.next(this.ingredients.slice());
     }
 }
