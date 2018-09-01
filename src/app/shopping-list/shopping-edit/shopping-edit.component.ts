77455974dc2ba79ee99112678333549520e4b8a6
shinylei <walei@umich.edu>
2018-09-02 04:30:03 +0800

    use ngModel and add validation for adding ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,19 +9,15 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInoutRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-
   constructor(private shoppinglistService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    this.shoppinglistService.addIngredient(
-      new Ingredient(this.nameInoutRef.nativeElement.value, 
-      this.amountInputRef.nativeElement.value)
-    );
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    this.shoppinglistService.addIngredient(newIngredient);
   }
 
 }
