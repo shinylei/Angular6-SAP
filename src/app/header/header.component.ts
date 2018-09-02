44a2e2f73197db08a0873f957b36828b97c87ba0
shinylei <walei@umich.edu>
2018-09-03 03:15:32 +0800

    check authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,8 @@ import { Response } from '@angular/http';
 })
 export class HeaderComponent implements OnInit {
   
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, 
+    private authService: AuthService) { }
 
   ngOnInit() {
   }
