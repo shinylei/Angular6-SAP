45c0dd2b4bbaa39e4c52f6b0e12f7e39075c9b63
shinylei <walei@umich.edu>
2018-09-03 03:08:45 +0800

    require and send token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from '@angular/http';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipes-list/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {
+    constructor(private http: Http, private recipeService: RecipeService,
+    private authService: AuthService) {
     }
 
     storeRecipes() {
-        return this.http.put('https://ng-recipe-book-44680.firebaseio.com/recipes.json', 
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-44680.firebaseio.com/recipes.json?auth=' + token, 
         this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-44680.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        this.http.get('https://ng-recipe-book-44680.firebaseio.com/recipes.json?auth=' + token)
         .subscribe(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
